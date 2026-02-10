14dd41557097587c0ad10f724c8332f89c098709
Kim Vernier <krrvernier@gmail.com>
2026-02-10 20:54:36 -0300

    automação do contato

---
+++ cypress/e2e/contato.cy.js
@@ -0,0 +1,47 @@
+describe('Funcionalidade contato', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/index.html')
+  });
+  
+  it('Deve preencher formulario de contato com sucesso', () => {
+    cy.get('[name="name"]').type('Ana Silva')
+    cy.get('[name="email"]').type('anasilva@teste.com')
+    cy.get('[name="subject"]').select('Dúvidas Gerais')
+    cy.get('[name="message"]').type('Mensagem de teste')
+    cy.get('#btn-submit').click()
+    cy.contains('Contato enviado com sucesso!').should('exist')
+  })
+
+  it('Deve validar mensagem de erro ao enviar formulario sem preencher nome', () => {
+    cy.get('[name="email"]').type('anasilva@teste.com')
+    cy.get('[name="subject"]').select('Dúvidas Gerais')
+    cy.get('[name="message"]').type('Mensagem de teste')
+    cy.get('#btn-submit').click()
+    cy.contains('Por favor, preencha o campo Nome').should('exist')
+  });
+
+  it('Deve validar mensagem de erro ao enviar formulario sem preencher e-mail', () => {
+    cy.get('[name="name"]').type('Ana Silva')
+    cy.get('[name="subject"]').select('Dúvidas Gerais')
+    cy.get('[name="message"]').type('Mensagem de teste')
+    cy.get('#btn-submit').click()
+    cy.contains('Por favor, preencha o campo E-mail').should('exist')
+  });
+
+  it('Deve validar mensagem de erro ao enviar formulario sem selecionar assunto', () => {
+    cy.get('[name="name"]').type('Ana Silva')
+    cy.get('[name="email"]').type('anasilva@teste.com')
+    cy.get('[name="message"]').type('Mensagem de teste')
+    cy.get('#btn-submit').click()
+    cy.contains('Por favor, selecione o Assunto').should('exist')
+  });
+
+  it('Deve validar mensagem de erro ao enviar formulario sem preencher a mensagem', () => {
+    cy.get('[name="name"]').type('Ana Silva')
+    cy.get('[name="email"]').type('anasilva@teste.com')
+    cy.get('[name="subject"]').select('Dúvidas Gerais')
+    cy.get('#btn-submit').click()
+    cy.contains('Por favor, escreva sua Mensagem').should('exist')
+  });
+})
+
